4185fc286ace969cfa63ede9608f7f9b80c04d06
Krishan Koenig <krishan.koenig@googlemail.com>
2019-04-01 18:50:17 +0200

    outsource fatal logs into util file

--- utils/error.go
+++ utils/error.go
@@ -1,9 +1,18 @@
 package utils
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
-func exitOnError(err error) {
+// ExitOnError log err and exits app
+func ExitOnError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// ExitOnNewError creates a new error from string and logs it
+func ExitOnNewError(message string) {
+	ExitOnError(errors.New(message))
 }
